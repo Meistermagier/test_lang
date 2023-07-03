2126500c861e2fede4498b55dd911102c8a8b31a
Tom Ihro <diesernameistgeheim@gmail.com>
2023-07-03 21:59:34 +0200

    implemented number type

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Number(pub i32);
+
+impl Number {
+    pub fn new(s: &str) -> Self {
+        Self(s.parse().unwrap())
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_number() {
+        assert_eq!(Number::new("123"), Number(123))
+    }
+}
